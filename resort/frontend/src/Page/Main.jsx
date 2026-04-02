da0edd3bca22b7eeed2dff8f4b5fba581b991c54
kim44690829-collab <kim44690829@gmail.com>
2026-04-02 14:34:34 +0900

    2026-04-02 update2

--- resort/frontend/src/Page/Main.jsx
+++ resort/frontend/src/Page/Main.jsx
@@ -523,7 +523,7 @@ export default function Main(){
                     <i className="fa-solid fa-angle-right"></i>
                 </button>
                 {eventBennerImg.slice(eventImgS,eventImgE).map((item, index) => (
-                    <Link to='/helpCenter' onClick={() => {window.scrollTo(0,0); setListType(2);}} key={index}>
+                    <Link to='/helpCenter2' onClick={() => {window.scrollTo(0,0); setListType(2);}} key={index}>
                         <img src={item} alt='eventBennerImg' className='event' key={index} />
                     </Link>
                 ))}
